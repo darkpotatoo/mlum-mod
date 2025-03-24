61c77571f0c4b8b6318cdac5316d4415b26d3017
darkpotatoo <darkpotatoben@gmail.com>
2025-03-24 13:24:04 -0400

    soon to be working iteminfo screen

--- src/client/java/me/darkpotatoo/mlumm/client/Iteminfo.java
+++ src/client/java/me/darkpotatoo/mlumm/client/Iteminfo.java
@@ -1,7 +1,6 @@
 package me.darkpotatoo.mlumm.client;
 
 import com.mojang.logging.LogUtils;
-import me.darkpotatoo.mlumm.client.mixins.HandledScreenMixin;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.item.ItemStack;
 import org.slf4j.Logger;
@@ -9,18 +8,18 @@ import org.slf4j.Logger;
 public class Iteminfo {
 
     private static final Logger LOGGER = LogUtils.getLogger();
-    private static ItemStack selecteditem;
+    private static ItemStack selectedItem;
 
     public static void runItemInfoKey() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (MlummClient.GetItemInfoKey.wasPressed()) {
-                return;
+                if (selectedItem != null) { attemptItemInfo(null); }
             }});}
 
     /** Pass {@code null} as an argument to display iteminfo; pass an {@code ItemStack} object to set the var */
     public static void attemptItemInfo(ItemStack item) {
-        if (item != null) { selecteditem = item; }
-        if (item == null) { displayIteminfoFromGUI(selecteditem); }
+        if (item != null) { selectedItem = item; }
+        if (item == null) { displayIteminfoFromGUI(selectedItem); }
     }
 
     public static void displayIteminfoFromGUI(ItemStack item) {
