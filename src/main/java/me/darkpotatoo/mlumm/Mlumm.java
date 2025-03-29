d12076ad9820e51fa04d7f35f4e095d0126b5e16
darkpotatoo <darkpotatoben@gmail.com>
2025-03-29 15:30:19 -0400

    tiny fixes

--- src/main/java/me/darkpotatoo/mlumm/Mlumm.java
+++ src/main/java/me/darkpotatoo/mlumm/Mlumm.java
@@ -6,10 +6,8 @@ import org.slf4j.Logger;
 
 public class Mlumm implements ModInitializer {
 
-    private static final Logger LOGGER = LogUtils.getLogger();
-
     @Override
     public void onInitialize() {
-        LOGGER.warn("i am hacker and it is too late for your computer muhahahahhahaha");
+
     }
 }
