12d766ee6b41f37d947bb1acda11e70bfc909d3b
darkpotatoo <darkpotatoben@gmail.com>
2025-03-29 11:27:51 -0400

    test

--- src/client/java/me/darkpotatoo/mlumm/client/mixins/DamageMixin.java
+++ src/client/java/me/darkpotatoo/mlumm/client/mixins/DamageMixin.java
@@ -1,7 +1,10 @@
 package me.darkpotatoo.mlumm.client.mixins;
 
 import me.darkpotatoo.mlumm.client.MlummClient;
+import me.darkpotatoo.mlumm.client.statistical.RiotTracker;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.LivingEntity;
@@ -23,11 +26,21 @@ public abstract class DamageMixin extends LivingEntity {
         if (amount != 0) {
             MlummClient.combatTicks = 100;
         }
+        if (RiotTracker.isEnabled) {
+            RiotTracker.damageTaken += amount;
+        }
     }
 
     @Inject(method = "attack", at = @At("HEAD"))
     private void onAttack(Entity target, CallbackInfo ci) {
         MlummClient.combatTicks = 100;
+        if (RiotTracker.isEnabled) {
+            MinecraftClient client = MinecraftClient.getInstance();
+            float dmg = this.isUsingRiptide() ? this.riptideAttackDamage : (float) getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
+            DamageSource dmgSource = this.getDamageSources().playerAttack(client.player);
+            RiotTracker.damageDealt += client.player.getWeaponStack().getItem().getBonusAttackDamage(target, dmg, dmgSource);
+            System.out.println(RiotTracker.damageDealt);
+        }
     }
 
 }
---
+++ src/client/java/me/darkpotatoo/mlumm/client/statistical/RiotTracker.java
@@ -0,0 +1,10 @@
+package me.darkpotatoo.mlumm.client.statistical;
+
+public class RiotTracker {
+
+    public static boolean isEnabled = true;
+    public static double damageDealt = 0;
+    public static double damageTaken = 0;
+
+
+}
