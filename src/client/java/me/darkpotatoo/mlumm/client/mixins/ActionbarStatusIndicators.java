ad98e1725822e2fd0905b11ecdd29ad65795305c
darkpotatoo <darkpotatoben@gmail.com>
2025-03-29 13:09:01 -0400

    wierd commit thingy (didnt commit with other commit)

--- src/client/java/me/darkpotatoo/mlumm/client/mixins/ActionbarStatusIndicators.java
+++ src/client/java/me/darkpotatoo/mlumm/client/mixins/ActionbarStatusIndicators.java
@@ -1,11 +1,12 @@
 package me.darkpotatoo.mlumm.client.mixins;
 
+import me.darkpotatoo.mlumm.client.Configuration;
 import me.darkpotatoo.mlumm.client.MlummClient;
 import me.darkpotatoo.mlumm.client.statistical.ChocolateStats;
 import me.darkpotatoo.mlumm.client.statistical.RiotTracker;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.network.message.MessageHandler;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,22 +14,23 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
-public class InGameHudMixin {
+public class ActionbarStatusIndicators {
 
     private boolean isUpdatingOverlayMessage = false;
+    Configuration config = AutoConfig.getConfigHolder(Configuration.class).getConfig();
 
     @Inject(method = "setOverlayMessage", at = @At("TAIL"))
     private void onSetOverlayMessage(Text message, boolean overlay, CallbackInfo ci) {
-        if (!isUpdatingOverlayMessage && message.getString().contains("Period:")) {
+        if (!isUpdatingOverlayMessage && message.getString().contains("Period:") && config.actionbar_status) {
             isUpdatingOverlayMessage = true;
             if (MlummClient.combatTicks > 0) {
                 message = Text.of(message.getString() + " §8| §6Combat Delay: §e" + (MlummClient.combatTicks / 20) + "s");
             }
             if (RiotTracker.isEnabled) {
-                message = Text.of(message.getString() + "§8| §eTracking Riot");
+                message = Text.of(message.getString() + " §8| §eTracking Riot");
             }
             if (ChocolateStats.isEnabled) {
-                message = Text.of(message.getString() + "§8| §eTracking Chocolate");
+                message = Text.of(message.getString() + " §8| §eTracking Chocolate");
             }
             MinecraftClient client = MinecraftClient.getInstance();
             client.inGameHud.setOverlayMessage(message, false);
